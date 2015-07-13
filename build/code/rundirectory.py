19ead7e3338a47bbcc21b06c3fec0b48b6ec1e33
Steve Lawford <steve_lawford@yahoo.co.uk>
2015-07-13 19:51:00 +0200

    Clean up code
     - uncomment production code

--- build/code/rundirectory.py
+++ build/code/rundirectory.py
@@ -85,70 +85,70 @@ def clear_bin_input():
     
     return None
     
-#manual_transfer_reminder()
-#
-#print
-#print 'clear contents of \output and \\temp and \input'
-#    
-#clear_output_temp_input()
-#
-##test_run = True parses Coupon or Ticket for one quarter only
-##security = True considers first (security_max) lines only
-#
-#parse_options = {}
-#parse_options['test_run'] = True
-#parse_options['security'] = False
-#parse_options['security_max'] = 10000
-#parse_options['test_periods'] = ([2010], [1])
-#parse_options['full_periods'] = (xrange(1993, 2014, 1), xrange(1, 5))
-#
-#print
-#print 'parse DB1B Coupon data from .zip to coupon_year_quarter.bin'
-#
-#parse_coupon_csv.wrapper(**parse_options)
-#
-#print
-#print 'clear \\temp'
-#
-#clear_temp()
-#
-#print
-#print 'parse DB1B Ticket data from .zip to coupon_year_quarter.bin'
-#
-#parse_ticket_csv.wrapper(**parse_options)
-#
-#print
-#print 'clear \\temp'
-#
-#clear_temp()
-#
-#print
-#print 'move .bin files from \\output to \\input'
-#    
-#move_bin_output_to_input()
-#
-#print
-#print 'merge Coupon and Ticket .bin files to Itinerary'
-#
-#merge_coupon_ticket.wrapper(**parse_options)
-#
-#print 'move all files in \\input to \\temp'
-#    
-#move_all_input_to_temp()
-#
-#print
-#print 'move .bin files from \\output to \\input'
-#
-#move_bin_output_to_input()
-#
-#print 'aggregate itinerary*.bin to route-level'
-#
-#aggregate_route_level.wrapper(**parse_options)
-#
-#print
-#print 'clear all .bin files from \\input'
-#    
-#clear_bin_input()
+manual_transfer_reminder()
+
+print
+print 'clear contents of \output and \\temp and \input'
+    
+clear_output_temp_input()
+
+#test_run = True parses Coupon or Ticket for one quarter only
+#security = True considers first (security_max) lines only
+
+parse_options = {}
+parse_options['test_run'] = True
+parse_options['security'] = False
+parse_options['security_max'] = 10000
+parse_options['test_periods'] = ([2010], [1])
+parse_options['full_periods'] = (xrange(1993, 2014, 1), xrange(1, 5))
+
+print
+print 'parse DB1B Coupon data from .zip to coupon_year_quarter.bin'
+
+parse_coupon_csv.wrapper(**parse_options)
+
+print
+print 'clear \\temp'
+
+clear_temp()
+
+print
+print 'parse DB1B Ticket data from .zip to coupon_year_quarter.bin'
+
+parse_ticket_csv.wrapper(**parse_options)
+
+print
+print 'clear \\temp'
+
+clear_temp()
+
+print
+print 'move .bin files from \\output to \\input'
+    
+move_bin_output_to_input()
+
+print
+print 'merge Coupon and Ticket .bin files to Itinerary'
+
+merge_coupon_ticket.wrapper(**parse_options)
+
+print 'move all files in \\input to \\temp'
+    
+move_all_input_to_temp()
+
+print
+print 'move .bin files from \\output to \\input'
+
+move_bin_output_to_input()
+
+print 'aggregate itinerary*.bin to route-level'
+
+aggregate_route_level.wrapper(**parse_options)
+
+print
+print 'clear all .bin files from \\input'
+    
+clear_bin_input()
 
 print
 print 'parse T-100 .csv files to .bin, save to \\temp'
