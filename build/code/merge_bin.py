f9e58d6c9579de0f9616be28e631da4c1ef6f125
Steve Lawford <steve_lawford@yahoo.co.uk>
2015-08-25 18:38:32 +0200

    Merge data_year_quarter.bin files
     - comment out production code for testing

---
+++ build/code/merge_bin.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import cPickle
+
+def merge(src_data, yyyy, q, full_data):
+        
+    print
+    print '[loading]', src_data
+        
+    f = open(src_data, 'r')
+    data = cPickle.load(f)
+    f.close()
+
+#    http://stackoverflow.com/questions/38987/how-can-i-merge-two-python-dictionaries-in-a-single-expression
+        
+    full_data.update(data)
+    
+    del data    
+        
+    return full_data
+
+def wrapper(test_run, test_periods, full_periods, security = None, security_max = None):
+    
+    full_data = {}
+    
+    if test_run:
+        
+        year_list = test_periods[0]
+        quarter_list = test_periods[1]
+        
+    else:
+                
+        year_list = full_periods[0]
+        quarter_list = full_periods[1]    
+    
+    for year in year_list:
+        for quarter in quarter_list:
+
+            src_data = '..\\output\\data_' + str(year) + '_' + str(quarter) + '.bin'
+            
+            try:
+                            
+                full_data = merge(src_data, year, quarter, full_data)
+                
+            except IOError:
+
+                raise IOError('requested data unavailable: year ' + str(year) + ', quarter ' + str(quarter))
+    
+    dst_full_data = '..\\output\\data_full.bin'    
+    
+    print
+    print '[saving]', dst_full_data    
+    
+    f = open(dst_full_data, 'wb')
+    cPickle.dump(full_data, f)
+    f.close()
+
+    del full_data
+       
+    return None
--- build/code/rundirectory.py
+++ build/code/rundirectory.py
@@ -10,6 +10,7 @@ import parse_t100
 import parse_population_by_msa
 import parse_airport_coordinates
 import parse_gdp_by_msa
+import merge_bin
 
 def manual_transfer_reminder():
 
@@ -37,76 +38,81 @@ def clear_output_temp_input():
     
     return None
     
-manual_transfer_reminder()
-
-print
-print 'clear contents of \output and \\temp and \input'
-    
-clear_output_temp_input()
-
-print 'test_run = True parses Coupon or Ticket for one quarter only'
-print 'security = True considers first (security_max) lines only'
+#manual_transfer_reminder()
+#
+#print
+#print 'clear contents of \output and \\temp and \input'
+#    
+#clear_output_temp_input()
+#
+#print 'test_run = True parses Coupon or Ticket for one quarter only'
+#print 'security = True considers first (security_max) lines only'
 
 parse_options = {}
-parse_options['test_run'] = True
+parse_options['test_run'] = False
 parse_options['security'] = False
 parse_options['security_max'] = 10000
 parse_options['test_periods'] = ([2010], [1])
 parse_options['full_periods'] = (xrange(1993, 2014, 1), xrange(1, 5))
 
+#print
+#print 'parse DB1B Coupon data from .zip to coupon_year_quarter.bin'
+#
+#parse_coupon_csv.wrapper(**parse_options)
+#
+#print
+#print 'parse DB1B Ticket data from .zip to coupon_year_quarter.bin'
+#
+#parse_ticket_csv.wrapper(**parse_options)
+#
+#print
+#print 'merge Coupon and Ticket .bin files to Itinerary'
+#
+#merge_coupon_ticket.wrapper(**parse_options)
+#
+#print
+#print 'aggregate itinerary*.bin to route-level'
+#
+#aggregate_route_level.wrapper(**parse_options)
+#
+#print
+#print 'parse T-100 .csv files to .bin, save to \\temp'
+#print '** note that raw T-100 .zip and .csv must be renamed as yyyy_*.* before use **'
+#
+#parse_t100.wrapper(**parse_options)
+#
+#print
+#print 'parse regional population (by MSA) data'
+#
+#parse_population_by_msa.parse()
+#
+#print
+#print 'parse airport coordinates'
+#
+#parse_airport_coordinates.parse()
+#
+#print
+#print 'parse regional GDP (by MSA) data'
+#
+#parse_gdp_by_msa.parse()
+#
+#import build_dataset
+#import convert_bin_to_text
+#
+#print
+#print 'filter + add variables to data dictionary'
+#
+#build_dataset.wrapper(**parse_options)
+#
+#print
+#print 'convert .bin to .txt'
+#
+#convert_bin_to_text.wrapper(**parse_options)
+
 print
-print 'parse DB1B Coupon data from .zip to coupon_year_quarter.bin'
+print 'merge .bin files'
 
-parse_coupon_csv.wrapper(**parse_options)
-
-print
-print 'parse DB1B Ticket data from .zip to coupon_year_quarter.bin'
-
-parse_ticket_csv.wrapper(**parse_options)
-
-print
-print 'merge Coupon and Ticket .bin files to Itinerary'
-
-merge_coupon_ticket.wrapper(**parse_options)
-
-print
-print 'aggregate itinerary*.bin to route-level'
-
-aggregate_route_level.wrapper(**parse_options)
-
-print
-print 'parse T-100 .csv files to .bin, save to \\temp'
-print '** note that raw T-100 .zip and .csv must be renamed as yyyy_*.* before use **'
-
-parse_t100.wrapper(**parse_options)
-
-print
-print 'parse regional population (by MSA) data'
-
-parse_population_by_msa.parse()
-
-print
-print 'parse airport coordinates'
-
-parse_airport_coordinates.parse()
-
-print
-print 'parse regional GDP (by MSA) data'
-
-parse_gdp_by_msa.parse()
-
-import build_dataset
-import convert_bin_to_text
-
-print
-print 'filter + add variables to data dictionary'
-
-build_dataset.wrapper(**parse_options)
-
-print
-print 'convert .bin to .txt'
-
-convert_bin_to_text.wrapper(**parse_options)
+merge_bin.wrapper(**parse_options)
 
 print
 print 'move pyc files (byte code) from \code to \\temp'
